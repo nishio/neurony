fca217c8cae502b5d8f2ba6ff54800d5958d1041
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2011-12-27 17:11:03 +0900

    both work

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 # -*- encoding: utf-8 -*-
 
 import numpy as np
-from random import shuffle
 
 class CharMapper(object):
     def __init__(self, chars):
@@ -15,6 +14,37 @@ class CharMapper(object):
     def to_obj(self, i):
         return self.chars[i]
 
+    def set(self, target, chars):
+        target.clear()
+        for c in chars:
+            i = self.to_int(c)
+            target.data[i] = 1.0
+
+    def get(self, target):
+        i = target.find_winner()
+        return self.to_obj(i)
+            
+
+class LEDMapper(object):
+    size = 25
+    def set(self, target, pattern):
+        target.clear()
+        pattern = pattern.replace("\n", "")
+        for i in range(len(pattern)):
+            if pattern[i] == "+":
+                target.data[i] = 1.0
+
+    def get(self, target):
+        buf = []
+        for i in range(5):
+            for j in range(5):
+                if target.data[i * 5 + j] > 0.5:
+                    c = "+"
+                else:
+                    c = "."
+                buf.append(c)
+            buf.append("\n")
+        return "".join(buf)
 
 class Neurons(object):
     def __init__(self, size=100, data=None, mapper=None):
@@ -27,7 +57,6 @@ class Neurons(object):
         if data:
             self.data = data
         else:
-            #self.data = np.random.random(self.size)
             self.data = np.repeat(0.5, self.size)
 
     def find_winner(self):
@@ -51,17 +80,11 @@ class Neurons(object):
         self.clear()
         self.data += 1
 
-    def set(self, chars, mapper=None):
-        if not mapper: mapper = self.mapper
-        self.clear()
-        for c in chars:
-            i = mapper.to_int(c)
-            self.data[i] = 1.0
+    def set(self, *args, **kw):
+        self.mapper.set(self, *args, **kw)
 
-    def get(self, mapper=None):
-        if not mapper: mapper = self.mapper
-        i = self.find_winner()
-        return mapper.to_obj(i)
+    def get(self, *args, **kw):
+        return self.mapper.get(self, *args, **kw)
 
 
 EPS = 0.0001 # small value to avoid zero-division
@@ -73,55 +96,64 @@ class Network(object):
         self.src = src
         self.dst = dst
         self.weight = normalize_prob(
-            #np.random.random((len(src.data), len(dst.data))))
             np.ones((src.size, dst.size)))
 
-    def learn(self, alpha=0.1):
+    def learn(self, alpha=0.1, both=False):
         update = np.outer(self.src.data, self.dst.data)
         self.weight[self.src.data > 0.5] *= 1 - alpha
-#        self.weight[self.src.data > 0.5] += update[self.src.data > 0.5] * alpha
         self.weight[self.src.data > 0.5] += self.dst.data * alpha
-
-        self.weight[self.src.data < 0.5] *= 1 - alpha
-        self.weight[self.src.data < 0.5] += (1 - self.dst.data) * alpha
+        if both:
+            self.weight[self.src.data < 0.5] *= 1 - alpha
+            self.weight[self.src.data < 0.5] += (1 - self.dst.data) * alpha
 
     def propagate(self):
         self.dst.data += self.src.data.dot(self.weight)
 
 
+DATA = """
++++++
++...+
++...+
++...+
++++++
 
-DATA = u"""
-りんご
-青りんご
-りんごジュース
-みかん
-オレンジ
-オレンジジュース
-ポンジュース
-""".strip()
+..+..
+..+..
+..+..
+..+..
+..+..
 
-DATA = u"""
-うさぎ
-ねずみ
-かささぎ
-ねこ
-カッパ
-パンダ
-ダルメシアン
-""".strip()
+++++.
++..+.
++..+.
++..+.
+++++.
 
+..+..
+.++..
+..+..
+..+..
+..+..
 
-CHARS = set(DATA)
-CHARS.remove("\n")
-CHAR_MAP = CharMapper(CHARS)
-DATA = DATA.split()
+.++++
+.+..+
+.+..+
+.+..+
+.++++
 
-input = Neurons(mapper=CharMapper(CHARS))
-output = Neurons(mapper=CharMapper("ox"))
+..+..
+.++..
+..+..
+..+..
+.+++.
+""".strip().split("\n\n")
+
+input = Neurons(mapper=LEDMapper())
+output = Neurons(mapper=CharMapper("01"))
 net = Network(input, output)
 
 def perceptron():
-    answer = "oooxxxx"
+    answer = "010101"
     for i in range(100):
         for d, ans in zip(DATA, answer):
             input.set(d)
@@ -129,25 +161,25 @@ def perceptron():
             net.learn()
 
     for d, ans in zip(DATA, answer):
+        output.clear()
         input.set(d)
         net.propagate()
         print d, ans, output.get()
+        print
 
 
 def kmeans():
-    for d in DATA:
-        input.set(d)
-        output.activate()
-        net.learn()
-
     for i in range(100):
         for d in DATA:
             input.set(d)
             net.propagate()
             output.winner_takes_all()
-            net.learn()
+            net.learn(both=True)
 
     for d in DATA:
+        output.clear()
         input.set(d)
         net.propagate() 
         print d, output.get()
+        print
+
