533901d9cd8243bf87ada6b9da353f6d68bd2970
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2012-06-25 14:47:58 +0900

    ngram

---
+++ ngram.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from collections import defaultdict, Counter, deque
+class NGram(object):
+    """
+    >>> ngram = NGram(3)
+    >>> ngram.feed_all("ABABABABABABAB")
+    >>> ngram.expect()
+    'A'
+    >>> ngram.feed('A')
+    >>> ngram.expect()
+    'B'
+    """
+    def __init__(self, n):
+        self.n = n
+        self.data = defaultdict(Counter)
+        self.backlog = deque(["BOS"] * n)
+        self.freq_counter = Counter()
+
+    def feed(self, c):
+        self.data[tuple(self.backlog)].update([c])
+        self.backlog.append(c)
+        self.backlog.popleft()
+        self.freq_counter.update([c])
+
+    def feed_all(self, string):
+        for c in string:
+            self.feed(c)
+
+    def expect(self):
+        mc = self.data[tuple(self.backlog)].most_common(1)
+        if len(mc) > 0:
+            return mc[0][0]
+        # TODO smoothing
+        mc = self.data[tuple(self.backlog)].most_common(1)
+        return mc[0][0]
+
+def _test():
+    import doctest
+    doctest.testmod()
+
+if __name__ == "__main__":
+    _test()
